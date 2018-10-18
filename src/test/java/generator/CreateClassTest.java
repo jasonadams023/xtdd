b76515b651ed0b339dfccd6a5fca6429779f1c95
Jabox2 <jabox2@gmail.com>
2018-10-18 11:46:17 -0500

    Removes old tests for CreateClass

--- src/test/java/generator/CreateClassTest.java
+++ src/test/java/generator/CreateClassTest.java
@@ -1,116 +1,25 @@
 package generator;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CreateClassTest {
-    private File exampleDirectory = new File("./example");
-    private File exampleSourceDirectory = new File(exampleDirectory.getPath() + "/src");
-
-    @AfterAll
-    @BeforeAll
-    void cleanup() {
-        File directory = exampleSourceDirectory;
-        for(File file: directory.listFiles()) {
-            if (!file.isDirectory()) {
-                file.delete();
-            }
-        }
-    }
-
-    @Test
-    void shouldWriteToFolder() {
-        String className = "Example";
-        File testFile = new File(exampleDirectory.getPath() + "/test/" + className + "Test.java");
-
-        Generator generator = new Generator(exampleDirectory);
-        Generator generatorSpy = spy(generator);
-        List<String> output = new ArrayList<>();
-        willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
-
-        generatorSpy.createClass(testFile);
-
-        assertTrue(exampleSourceDirectory.list().length > 0);
-    }
-
-    @Test
-    void shouldCreateJavaFile() {
-        String className = "Example";
-        File testFile = new File(exampleDirectory.getPath() + "/test/" + className + "Test.java");
-
-        Generator generator = new Generator(exampleDirectory);
-        Generator generatorSpy = spy(generator);
-        List<String> output = new ArrayList<>();
-        willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
-
-        generatorSpy.createClass(testFile);
-        File file = new File(exampleSourceDirectory.getPath() + "/Example.java");
-        assertTrue(file.exists());
-    }
-
-    @Test
-    void shouldGenerateClass() {
-        String className = "Example";
-        File testFile = new File(exampleDirectory.getPath() + "/test/" + className + "Test.java");
-
-        Generator generator = new Generator(exampleDirectory);
-        Generator generatorSpy = spy(generator);
-        List<String> output = new ArrayList<>();
-        willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
-
-        generatorSpy.createClass(testFile);
-
-        String data = "";
-        try {
-            data = new String(Files.readAllBytes(Paths.get(exampleSourceDirectory.getPath() + "/" + className + ".java")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertTrue(data.contains("class " + className + " {"));
-    }
-
-    @Test
-    void shouldGenerateDifferentClass() {
-        String className = "Different";
-        File testFile = new File(exampleDirectory.getPath() + "/test/" + className + "Test.java");
-
-        Generator generator = new Generator(exampleDirectory);
-        Generator generatorSpy = spy(generator);
-        List<String> output = new ArrayList<>();
-        willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
-
-        generatorSpy.createClass(testFile);
-
-        String data = "";
-        try {
-            data = new String(Files.readAllBytes(Paths.get(exampleSourceDirectory.getPath() + "/" + className + ".java")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertTrue(data.contains("class " + className + " {"));
-    }
+    private File directory = new File("./someDirectory");
 
     @Test
     void should_CallWriteFileWithClassName() {
-        File testFile = new File(exampleDirectory.getPath() + "/test/ExampleTest.java");
-        Generator generator = new Generator(exampleDirectory);
+        File testFile = new File(directory.getPath() + "/test/ExampleTest.java");
+        Generator generator = new Generator(directory);
         Generator generatorSpy = spy(generator);
 
         willReturn("Example").given(generatorSpy).getClassName(testFile);
@@ -118,7 +27,7 @@ class CreateClassTest {
         List<String> output = new ArrayList<>();
         willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
 
-        Path expectedPath = Paths.get(exampleDirectory.getPath() + "/src/Example.java");
+        Path expectedPath = Paths.get(directory.getPath() + "/src/Example.java");
         String expectedInput = "class Example {\n}\n";
 
         doNothing().when(generatorSpy).writeFile(expectedPath, expectedInput);
@@ -130,8 +39,8 @@ class CreateClassTest {
 
     @Test
     void should_CallWriteFileWithDifferentClassName() {
-        File testFile = new File(exampleDirectory.getPath() + "/test/DifferentTest.java");
-        Generator generator = new Generator(exampleDirectory);
+        File testFile = new File(directory.getPath() + "/test/DifferentTest.java");
+        Generator generator = new Generator(directory);
         Generator generatorSpy = spy(generator);
 
         willReturn("Different").given(generatorSpy).getClassName(testFile);
@@ -139,7 +48,7 @@ class CreateClassTest {
         List<String> output = new ArrayList<>();
         willReturn(output).given(generatorSpy).getFunctionSignatures(testFile);
 
-        Path expectedPath = Paths.get(exampleDirectory.getPath() + "/src/Different.java");
+        Path expectedPath = Paths.get(directory.getPath() + "/src/Different.java");
         String expectedInput = "class Different {\n}\n";
 
         doNothing().when(generatorSpy).writeFile(expectedPath, expectedInput);
