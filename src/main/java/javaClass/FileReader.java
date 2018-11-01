59569f21ccf565b07d1ab01fbcd32e1ad4972ecc
Jabox2 <jabox2@gmail.com>
2018-11-01 11:41:36 -0500

    Removes FileReader

--- src/main/java/javaClass/FileReader.java
+++
@@ -1,9 +0,0 @@
-package javaClass;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-
-public interface FileReader {
-    List<String> readAllLines(Path path) throws IOException;
-}
