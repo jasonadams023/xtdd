cbea7b633b5b6a9a7abfb7b3e7fa0a92439071c3
Jason Adams <jasonadams023@gmail.com>
2019-08-12 13:04:09 -0500

    Extracts cleanup function for MainTest

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,27 +1,32 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MainTest {
     private File exampleDirectory = new File("./example");
     private File exampleSourceDirectory = new File(exampleDirectory.getPath() + "/src");
+
+    @BeforeEach
+    @AfterAll
+    void cleanup() {
+        for(File file: exampleSourceDirectory.listFiles()) {
+            if (!file.isDirectory()) {
+                file.delete();
+            }
+        }
+    }
 
     @Test
     void should_RunGenerate() {
         String[] args = new String[2];
         args[0] = exampleDirectory.toString();
 
-        if (exampleSourceDirectory.exists()) {
-            for (File file : exampleSourceDirectory.listFiles()) {
-                if (!file.isDirectory()) {
-                    file.delete();
-                }
-            }
-        }
-
         Main.main(args);
 
-        assert(exampleSourceDirectory.exists());
         assert(exampleSourceDirectory.listFiles().length > 0);
     }
 }
