b7b76ad9ce206873e93384cd13580f8faf2ade20
Jason Adams <jasonadams023@gmail.com>
2019-08-15 13:22:00 -0500

    Refactors VariableParser

--- src/main/java/parsing/variable/VariableParser.java
+++ src/main/java/parsing/variable/VariableParser.java
@@ -7,8 +7,9 @@ import java.util.regex.Pattern;
 
 public class VariableParser {
     public static Variable parseFromAssignment(String line) {
-        String className = extractClassName(line);
-        String valueString = extractValueString(line);
+        String writtenClassName = extractWrittenClassNameFromAssignment(line);
+        String className = getClassName(writtenClassName);
+        String valueString = extractValueStringFromAssignment(line);
         Object object = "";
 
         try {
@@ -20,14 +21,13 @@ public class VariableParser {
         return new Variable(object);
     }
 
-    public static Object parseFromAssert(String type, String line) {
+    public static Object parseFromAssert(String writtenClassName, String line) {
+        String value = extractValueStringFromAssert(line);
+        String className = getClassName(writtenClassName);
         Object object = "";
-        String args = line.split(Pattern.quote("("))[1];
-        String returnValueString = args.split(Pattern.quote(","))[0];
-        String value = returnValueString.replaceAll("\"", "");
 
         try {
-            object = dynamicallyCreateObject("java.lang." + type, value);
+            object = dynamicallyCreateObject(className, value);
         } catch (ReflectiveOperationException e) {
             System.out.println(e);
         }
@@ -39,12 +39,21 @@ public class VariableParser {
         return object;
     }
 
-    private static String extractClassName(String line) {
-        String type = line.trim().split(Pattern.quote(" "))[0];
+    private static String getClassName(String type) {
         return "java.lang." + type;
     }
 
-    private static String extractValueString(String line) {
+    private static String extractValueStringFromAssert(String line) {
+        String args = line.split(Pattern.quote("("))[1];
+        String returnValueString = args.split(Pattern.quote(","))[0];
+        return returnValueString.replaceAll("\"", "");
+    }
+
+    private static String extractWrittenClassNameFromAssignment(String line) {
+        return line.trim().split(Pattern.quote(" "))[0];
+    }
+
+    private static String extractValueStringFromAssignment(String line) {
         return line.split(Pattern.quote("="))[1].trim().split(Pattern.quote(";"))[0];
     }
 
