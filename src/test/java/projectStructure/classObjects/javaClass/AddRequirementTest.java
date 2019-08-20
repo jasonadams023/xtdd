c9e6c5fadbec2c196c73f3380fb3d10f00eaba60
Jason Adams <jasonadams023@gmail.com>
2019-08-20 16:37:16 -0500

    Extracts setup into prebuilt static values

--- src/test/java/projectStructure/classObjects/javaClass/AddRequirementTest.java
+++ src/test/java/projectStructure/classObjects/javaClass/AddRequirementTest.java
@@ -5,49 +5,47 @@ import projectStructure.functionObjects.functionRequirement.FunctionRequirement;
 import org.junit.jupiter.api.Test;
 import projectStructure.variable.Variable;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddRequirementTest {
+    private static Variable intOneVariable = Variable.create(1);
+    private static Variable intTwoVariable = Variable.create(2);
+    private static Variable[] intOneList = {intOneVariable};
+    private static Variable[] intTwoList = {intTwoVariable};
+    private static List<Variable> intOneInputs = Arrays.asList(intOneList);
+    private static List<Variable> intTwoInputs = Arrays.asList(intTwoList);
+    private static List<String> singleIntegerInputs = Collections.singletonList("Integer");
+    private static Signature voidNullSignature = new Signature("function", "void", null);
+    private static Signature intIntSignature = new Signature("function", "Integer", singleIntegerInputs);
+    private static FunctionRequirement nullRequirement = FunctionRequirement.create(voidNullSignature, null, null);
+
     @Test
     void should_AddFunction_BasedOnRequirement() {
-        Signature signature = new Signature("function", "void", null);
-        FunctionRequirement functionRequirement = FunctionRequirement.create(signature, null, null);
         JavaClass javaClass = new JavaClass("Example");
 
-        javaClass.addRequirement(functionRequirement);
+        javaClass.addRequirement(nullRequirement);
 
         assertEquals(1, javaClass.functions.size());
     }
 
     @Test
     void should_NotAddFunction_WhenItAlreadyExists() {
-        Signature signature = new Signature("function", "void", null);
-        FunctionRequirement functionRequirement = FunctionRequirement.create(signature, null, null);
         JavaClass javaClass = new JavaClass("Example");
 
-        javaClass.addRequirement(functionRequirement);
-        javaClass.addRequirement(functionRequirement);
+        javaClass.addRequirement(nullRequirement);
+        javaClass.addRequirement(nullRequirement);
 
         assertEquals(1, javaClass.functions.size());
     }
 
     @Test
-    void should_AddNewRequirement_ToFunction() {
-        List<String> inputTypes = new ArrayList<>();
-        inputTypes.add("Integer");
-        Signature signature = new Signature("function", "Integer", inputTypes);
-        List<Variable> inputs = new ArrayList<>();
-        inputs.add(Variable.create(1));
-        Variable returnValue = Variable.create(1);
-        FunctionRequirement functionRequirement = FunctionRequirement.create(signature, inputs, returnValue);
-
-        List<Variable> inputs2 = new ArrayList<>();
-        inputs2.add(Variable.create(1));
-        Variable returnValue2 = Variable.create(1);
-        FunctionRequirement functionRequirement2 = FunctionRequirement.create(signature, inputs2, returnValue2);
+    void should_AddNewRequirement_ToFunction3() {
+        FunctionRequirement functionRequirement = FunctionRequirement.create(intIntSignature, intOneInputs, intOneVariable);
+        FunctionRequirement functionRequirement2 = FunctionRequirement.create(intIntSignature, intTwoInputs, intTwoVariable);
 
         JavaClass javaClass = new JavaClass("Example");
 
